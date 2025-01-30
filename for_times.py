c6ca0dc62599ee9c7a7cb0b8c047129b79975e0f
R M GAYATHRI <gayathrirmgayathri@gmail.com>
2025-01-30 20:21:57 +0530

    Add files via upload

---
+++ for_times.py
@@ -0,0 +1,7 @@
+'''problem :Imagine you need to repeat a cheerful message. Write a program that uses a for 
+loop to print "ALL IS WELL" exactly twenty times. How will you set up your loop to 
+ensure this message appears the right number of times?'''
+
+
+for i in range(1,20): #starting from 0-20 , so it exactly prints 20 times.
+    print("ALL IS WELL")  #if starting from 1, it will print less than 1 time.
