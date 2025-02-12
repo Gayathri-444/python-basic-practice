6584c287ccfb37312167c4e3678ac8059aa33e61
R M GAYATHRI <gayathrirmgayathri@gmail.com>
2025-02-12 18:47:55 +0530

    Add files via upload

---
+++ printing_message.py
@@ -0,0 +1,12 @@
+#problem : For this problem, the prefilled code will contain a function. Write a function with two arguments that prints the message as expected.
+#sample : Akhil is 15 years old.
+
+def message(arg_1, arg_2):
+    message = name + " is " + a + " years old."
+    print(message)
+
+
+name = input("Enter the name: ")
+age = int(input("Enter the age: "))
+a = str(age)
+message(arg_1 = name, arg_2 = a)
