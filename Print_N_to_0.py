a9f1616b7d60aa74a9a853ef7df23f9c012b21a6
R M GAYATHRI <gayathrirmgayathri@gmail.com>
2025-02-22 20:00:38 +0530

    Add files via upload

---
+++ Print_N_to_0.py
@@ -0,0 +1,7 @@
+#problem : Write a program that reads a number N and print N numbers starting from 0.
+
+n = int(input("Enter the value of N : "))
+i = 0 
+for i in range(n):
+    i += 0 
+    print(i)
